957d22c504e973146fa6e2d2a498e5c63b044a4e
Marie-Adeline Marteau <marieadelinemarteau@gmail.com>
2025-02-27 16:05:25 +0100

    exercice movie

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listd in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
